c37a8a245a4ecbd4b4625481f8644f0cb8caf9e0
Vision goes Vegas <vgv_05fs18@tractor.ee.ethz.ch>
2018-02-28 15:55:46 +0100

    Added to Eclipse

---
+++ build/CMakeFiles/recognition.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/recognition.dir/image.cpp.o"
+  "CMakeFiles/recognition.dir/main.cpp.o"
+  "CMakeFiles/recognition.dir/geom.cpp.o"
+  "CMakeFiles/recognition.dir/card.cpp.o"
+  "recognition.pdb"
+  "recognition"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/recognition.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for recognition.
+# This may be replaced when dependencies are built.
