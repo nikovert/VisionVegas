838e9d612c50c10c81e40e5bbdae83fe26329b5b
Vision goes Vegas <vgv_05fs18@biwidl106.ee.ethz.ch>
2018-03-07 14:26:02 +0100

    Added BlobDetector class

---
+++ src/BlobDetector.cpp
@@ -0,0 +1,18 @@
+/*
+ * BlobDetector.cpp
+ *
+ *  Created on: Mar 7, 2018
+ *      Author: vgv_05fs18
+ */
+
+#include <BlobDetector.h>
+
+BlobDetector::BlobDetector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlobDetector::~BlobDetector() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/BlobDetector.h
@@ -0,0 +1,17 @@
+/*
+ * BlobDetector.h
+ *
+ *  Created on: Mar 7, 2018
+ *      Author: vgv_05fs18
+ */
+
+#ifndef BLOBDETECTOR_H_
+#define BLOBDETECTOR_H_
+
+class BlobDetector {
+public:
+	BlobDetector();
+	virtual ~BlobDetector();
+};
+
+#endif /* BLOBDETECTOR_H_ */
