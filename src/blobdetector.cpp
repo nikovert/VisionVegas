e131a2b9078af2618b0b7e74caa5c9518be63f10
nikovert <niko.vertovec@outlook.com>
2018-03-15 07:12:29 +0100

    fixed blob array not flushing itself;

--- src/blobdetector.cpp
+++ src/blobdetector.cpp
@@ -52,7 +52,7 @@ std::vector<BLOB> BlobDetector::findBlobs(Image& imb)
                     if(thresholded.at(x+1,y) == black || thresholded.at(x,y+1) == black){   //might be a new blob
                         blob_continuation = true;
                         if(x!= 0 && blobfield[x-1][y] > 0)         //check previous fields to see if it was a blob
-                            blobcount = blobfield[x][y];
+                            blobcount = blobfield[x-1][y];
                         else if(y!= 0 && blobfield[x][y-1] > 0) // might be ambiguous
                             blobcount = blobfield[x][y-1];
                         else
@@ -60,6 +60,7 @@ std::vector<BLOB> BlobDetector::findBlobs(Image& imb)
                         blobfield[x][y] = blobcount;
                     }
             }else{
+                blobfield[x][y] = 0; //might be ambiguius
                 if(blob_continuation){
                     blob_continuation = false;
                 }
@@ -83,7 +84,7 @@ std::vector<BLOB> BlobDetector::findBlobs(Image& imb)
     for (unsigned x = 0; x < blobed.width(); x++) {
         for (unsigned y = 0; y < blobed.height(); y++) {
             if(blobfield[x][y] > 0){
-                blobed.drawMarker(x, y, RGB(0, 150, 0), 1);
+                blobed.drawMarker(x, y, RGB(blobfield[x][y]%255, blobfield[x][y]/255, 0), 1);
             }
         }
     }
